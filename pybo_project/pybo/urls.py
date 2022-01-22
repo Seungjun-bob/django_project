13b9713196a8e67eed6026b738ce45ee494cf493
Seungjub-bob <rkdtmdwns0105@gmail.com>
2022-01-22 23:31:44 +0900

    templates

--- pybo_project/pybo/urls.py
+++ pybo_project/pybo/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'pybo' # 동일한 URL 별칭 사용의 중복을 막음, 네임스페이스를 의미
+
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='index'), # URL매핑에 name 속성을 부여(http://localhost:8000/pybo/)
+    path('<int:question_id>/', views.detail, name='detail')
 ]
--- pybo_project/pybo/views.py
+++ pybo_project/pybo/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+# render: 파이썬 데이터 템플릿에 적용하여 HTML로 변환
+# get_object_or_404: 404 오류 출력(요청한 페이지가 없는 경우)
+from django.shortcuts import render, get_object_or_404
+# from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    """
+    pybo 목록 출력
+    """
+    # 질문 목록 데이터 '-create_date'는 작성일시 역순(게시물을 최신으로 확인)
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+
+    # 'pybo/question_list.html' -> 템플릿 -> config/settings.py에 추가
+    return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    """
+    pybo 내용 출력
+    """
+    # 오류를 404오류로 출력
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
