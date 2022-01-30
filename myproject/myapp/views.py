0280cd4d975e9426c62e895787aa003f0880cf5c
Seungjub-bob <rkdtmdwns0105@gmail.com>
2022-01-30 22:32:55 +0900

    initial commit

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from django.views.decorators.csrf import csrf_exempt
 
+nextId = 4
 topics = [
     {'id':1, 'title':'routing', 'body':'Routing is ..'},
-    {'id':2, 'title':'view', 'body':'view is ..'},
-    {'id':3, 'title':'model', 'body':'model is ..'}
+    {'id':2, 'title':'view', 'body':'View is ..'},
+    {'id':3, 'title':'Model', 'body':'Model is ..'},
 ]
+
 def HTMLTemplate(articleTag):
     global topics
     ol = ''
@@ -42,11 +44,21 @@ def read(request, id):
 
 @csrf_exempt
 def create(request):
-    article = '''
-        <form action="/create/" method="POST">
-            <p><input type="text" name="title" placeholder="title"></p>
-            <p><textarea name="body" placeholder="body"></textarea></p>
-            <p><input type="submit"></p>
-        </form>
-    '''
-    return HttpResponse(HTMLTemplate(article))
+    global nextId
+    if request.method == 'GET':
+        article = '''
+            <form action="/create/" method="post">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p><textarea name="body" placeholder="body"></textarea></p>
+                <p><input type="submit"></p>
+            </form>
+        '''
+        return HttpResponse(HTMLTemplate(article))
+    elif request.method == 'POST':
+        title = request.POST['title']
+        body = request.POST['body']
+        newTopic = {"id":nextId, "title":title, "body":body}
+        topics.append(newTopic)
+        url = '/read/'+str(nextId)
+        nextId = nextId + 1
+        return redirect(url)
